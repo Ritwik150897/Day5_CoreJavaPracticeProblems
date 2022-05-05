7529453f212e91d1f6c1f9d0a1ded01966fba7b9
Ritwik <deshrutu@gmail.com>
2022-05-05 20:12:58 +0530

    Even Odd Program

---
+++ src/com/BridgeLabz/Day5/EvenOdd.java
@@ -0,0 +1,17 @@
+package com.BridgeLabz.Day5;
+
+import java.util.Scanner;
+
+public class EvenOdd {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a Number: ");
+        int n = sc.nextInt();
+
+        if(n % 2 == 0){
+            System.out.println(n + " is an Even Number.");
+        } else{
+            System.out.println(n + " is an Odd Number.");
+        }
+    }
+}
