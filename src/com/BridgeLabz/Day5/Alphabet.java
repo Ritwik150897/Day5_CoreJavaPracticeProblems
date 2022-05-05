cae75c629cf15f71c5888a59ea63d4728b9f6acb
Ritwik <deshrutu@gmail.com>
2022-05-05 20:23:27 +0530

    Alphabet Vowel Consonant Program

---
+++ src/com/BridgeLabz/Day5/Alphabet.java
@@ -0,0 +1,18 @@
+package com.BridgeLabz.Day5;
+
+import java.util.Scanner;
+
+public class Alphabet {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter an Alphabet: ");
+        char ch = sc.next().charAt(0);
+
+        if(ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U'
+                || ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u'){
+            System.out.println(ch + " Is a Vowel.");
+        }else {
+            System.out.println(ch + " Is a Consonant.");
+        }
+    }
+}
